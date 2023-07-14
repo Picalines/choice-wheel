505f1a8a45c04eb3a0ba1e5c3aed2a5ef32df57b
Picalines <picalines0@gmail.com>
2023-07-14 19:24:58 +0300

    github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from 'path';
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+	plugins: [svelte()],
+	base: "/choice-wheel/",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
